f6925d91e4468281c961b7c0ba5bafaf0dcae9ee
yangajila <griangajila2@gmail.com>
2022-11-19 23:43:23 +0800

    fiorst commit

---
+++ js/words.js
@@ -0,0 +1,217 @@
+let wordList = [{
+        word: "tyt",
+        hint: "it means you can spend as much time as you need in doing something, it is an abbreviation for take your time"
+    },
+    {
+        word: "brb",
+        hint: "this abbreviation use to indicate the speaker will return in a moment"
+    },
+    {
+        word: "asap",
+        hint: "this abbreviation or acronym, means something should be done fast as it is able to be done"
+    },
+    {
+        word: "ily",
+        hint: "this acronym shows the affirmation or affection or deep caring to someone. Someone's saying this word to show their love"
+    },
+    {
+        word: "otw",
+        hint: "this acronym means in the process of coming, going, or travelling and also about to come"
+    },
+    {
+        word: "yolo",
+        hint: "this abbreviation is basically an excuse used by people who lack patience in them. It is an encouragement for a person"
+    },
+    {
+        word: "pov",
+        hint: "this is a short term that refers to who is telling a story, or who is naarating it."
+    },
+    {
+        word: "pls",
+        hint: "an abbreviation to afford or give pleasure or satisfaction. It is use to ask something in a polite way"
+    },
+    {
+        word: "ttys",
+        hint: "this acronym can indicate that you hope or wish to speak with the other person sooner, rather that later"
+    },
+    {
+        word: "ty",
+        hint: "this acronym is used to express appreciation to someone for offering or giving something, for helping you"
+    },
+    {
+        word: "tbh",
+        hint: "thid abbreviation is about telling the truth or able to be trusted and not not a lie"
+    },
+    {
+        word: "jmo",
+        hint: "this abbreviation is about sharing his/her opinion to others"
+    },
+    {
+        word: "fomo",
+        hint: "this abbreviation means an emotional response to the belief that people are living better,or that omportant opportunities are being missed."
+    },
+    {
+        word: "afaik",
+        hint: "this acronym indicate that you are not absolutely sure of the statement you are about to make or have just made and you may be wrong"
+    },
+    {
+        word: "hth",
+        hint: "it is an acronym that means the person who is addressed should or must hope that it will help "
+    },
+    {
+        word: "bts",
+        hint: "this acronym means, it happens without most people knowing about it, especially when someting else is happenning publicly"
+    },
+    {
+        word: "nc",
+        hint: "this acronym indicates that the speaker does not choose to say nothing on the subject, and it is automatically invalidated"
+    },
+    {
+        word: "idc",
+        hint: "this acronym indicates that the speaker has no interest or emotional investment in the topic at hand"
+    },
+    {
+        word: "lmk",
+        hint: "this abbreviation means to keep me informed, tell me"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "aka",
+        hint: "it is an abbreviation that means as known or named at another time place"
+    },
+    {
+        word: "lol",
+        hint: "this abbreviation means that it is extremely funny laughing out loud moment"
+    },
+    {
+        word: "cpu",
+        hint: "this stands for the central processing unit of the computer and this is like a computer's brain"
+    },
+    {
+        word: "afa",
+        hint: "this acronym stands for all flash array, a grouping of flash memorydevices that helps boost performance"
+    },
+    {
+        word: "mac",
+        hint: "this is an abbreviation for Macintosh, which is a type of personal computer made by the apple computer company"
+    },
+    {
+        word: "pc",
+        hint: "this is the abbreviation for personal computer.It is originally referred to computers that were IBM compatible"
+    },
+    {
+        word: "ram",
+        hint: "this stands for random access memory, which is the space inside the computer that can be accessed at one time."
+    },
+    {
+        word: "rom",
+        hint: "this acronym is read only memory, which is the instruction for the computer and cannot be altered"
+    },
+    {
+        word: "pdf",
+        hint: "this represents the portable document format, which display files in a format that is ready for web."
+    },
+    {
+        word: "html",
+        hint: "hypertext markup language formarts information so it can be transported on the internet"
+    },
+    {
+        word: "http",
+        hint: "hypertext transfer protocol is a set of instruction for the software that controls the movement of files on the internet"
+    },
+    {
+        word: "url",
+        hint: "this is the uniform resource locator, which is a path to a certain file on the World Wide Web.It is what you may also call the web address"
+    },
+    {
+        word: "usb",
+        hint: "it is the universal serial box used for communications between certain devices"
+    },
+    {
+        word: "ftp",
+        hint: "this is a service called file transport protocol, which moves a file between computers using internet"
+    },
+    {
+        word: "lan",
+        hint: "this stands for local area network, which consists of the servers that your computer connects to inyour local area"
+    },
+    {
+        word: "sds",
+        hint: "this stands for software defined storage, a type of data storage that seperates the data from its associated software"
+    },
+    {
+        word: "os",
+        hint: "this is the operating system of the computer. It is the main program that runs on a computer and begins automatically when the computer is turned on"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "key",
+        hint: "small piece of metal"
+    },
+    {
+        word: "seo",
+        hint: "this is an acronym for search engine Optimization"
+    },
+    {
+        word: "url",
+        hint: "this is the uniform resource locator,which is a path to acertain file on the world wide web"
+    },
+    {
+        word: "ip",
+        hint: "it is an abbreviatio for internet protocols which is the set of rules that govern the system connected to the internet"
+    },
+    {
+        word: "rdma",
+        hint: "this stands for remote direct memory"
+    },
+    {
+        word: "lcd",
+        hint: "this stands for liquid crystal display, a type of computer screen"
+    },
+    {
+        word: "raid",
+        hint: "it is a type of storage that can be configured in different ways to provide a rebundant copy files, it stands for rebundant array of independent disks"
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
